2e2c7f9bbf5203b061721f997eccf6c62c9131cc
aamirubit <aamirsaleem167@gmail.com>
2020-10-14 20:04:37 +0500

    css completed

--- src/init.js
+++ src/init.js
@@ -1,18 +1,18 @@
-const TICK_RATE = 3000;
-
-function tick() {
-  console.log("tick", Date.now());
-}
+import game from "./gameState";
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
 
 async function init() {
   console.log("starting game");
+  initButtons(game.handleUserAction);
+
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
     const now = Date.now();
 
     if (nextTimeToTick <= now) {
-      tick();
+      game.tick();
       nextTimeToTick = now + TICK_RATE;
     }
     requestAnimationFrame(nextAnimationFrame);
